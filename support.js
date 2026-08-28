3eb580639dd5747810162dc871b1c60743bae50f
kprile <58675705+kprile@users.noreply.github.com>
2026-08-28 17:26:40 -0400

    Renovar caché del controlador estable

--- support.js
+++ support.js
@@ -2,11 +2,11 @@
 (() => {
   const css=document.createElement('link');
   css.rel='stylesheet';
-  css.href='./intro-experience.css?v=20260828d';
+  css.href='./intro-experience.css?v=20260828e';
   document.head.appendChild(css);
   document.write('<script src="./runtime.js"><\/script>');
   const s=document.createElement('script');
-  s.src='./intro-experience.js?v=20260828d';
+  s.src='./intro-experience.js?v=20260828e';
   s.async=false;
   document.head.appendChild(s);
 })();
